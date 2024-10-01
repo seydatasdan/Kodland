dba16b0d5cd303fc1c8d6205470d477d87e234ab
Şeyda Taşdan <55542610+seydatasdan@users.noreply.github.com>
2024-10-01 22:19:33 +0300

    Create password_developer.py

---
+++ password_developer.py
@@ -0,0 +1,13 @@
+import random
+
+char = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+char_lenght = int(input("Şifreniz kaç karakter olsun? "))
+
+password = ""   
+
+for i in range(char_lenght):
+    password += random.choice(char)
+
+
+print("Şifreniz:", password)   
